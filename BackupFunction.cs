8d3cdf0dc9f274a8ccf342d02e0763d74cbaa268
Gilson Ravaiani <gravaiani@yahoo.com.br>
2025-07-06 00:04:59 -0300

    Corrigindo arquivos

--- BackupFunction.cs
+++ BackupFunction.cs
@@ -1,51 +1,13 @@
-using System;
-using Azure.Storage.Blobs;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
-
-namespace BackupFunctionApp
+public class BackupFunction
 {
-    public class BackupFunction
+    [Function("BackupFunction")]
+    public void Run([TimerTrigger("0 0 2 * * *")] TimerInfo timer, FunctionContext context)
     {
-        [Function("BackupFunction")]
-        public void Run([TimerTrigger("0 0 2 * * *")] TimerInfo myTimer, FunctionContext context)
-        {
-            var log = context.GetLogger("BackupFunction");
-            log.LogInformation($"Backup iniciado: {DateTime.UtcNow}");
+        var log = context.GetLogger("BackupFunction");
+        log.LogInformation("Função iniciada");
 
-            var srcConnection = Environment.GetEnvironmentVariable("SRC_STORAGE_CONNECTION");
-            var destConnection = Environment.GetEnvironmentVariable("DEST_STORAGE_CONNECTION");
+        // lógica de backup aqui
 
-            var srcClient = new BlobServiceClient(srcConnection);
-            var destClient = new BlobServiceClient(destConnection);
-
-            var srcContainer = srcClient.GetBlobContainerClient("origem");
-            var destContainer = destClient.GetBlobContainerClient("backup");
-
-            foreach (var blob in srcContainer.GetBlobs())
-            {
-                var srcBlob = srcContainer.GetBlobClient(blob.Name);
-                var destBlob = destContainer.GetBlobClient(blob.Name);
-
-                var content = srcBlob.DownloadContent().Value.Content;
-                destBlob.Upload(content.ToStream(), overwrite: true);
-            }
-
-            log.LogInformation("Backup concluído!");
-        }
-
-        private class TimerTriggerAttribute : Attribute
-        {
-            private string v;
-
-            public TimerTriggerAttribute(string v)
-            {
-                this.v = v;
-            }
-        }
-    }
-
-    public class TimerInfo
-    {
+        log.LogInformation("Backup concluído!");
     }
 }
